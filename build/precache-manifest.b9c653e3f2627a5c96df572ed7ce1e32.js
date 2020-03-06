337a4f593a97bba8cf67f8caaf7771b27916d2ef
s3lcuk <s3lcukdemir@gmail.com>
2020-03-06 16:48:58 +0300

    routes

---
+++ build/precache-manifest.b9c653e3f2627a5c96df572ed7ce1e32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9fa01ece54f0deb9300f0f38580c7a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0db493fbc48cf65bf632",
+    "url": "/static/css/main.7e15a82a.chunk.css"
+  },
+  {
+    "revision": "473ac9b5280745672bf8",
+    "url": "/static/js/2.7850cbdd.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/static/js/2.7850cbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0db493fbc48cf65bf632",
+    "url": "/static/js/main.dacbec4f.chunk.js"
+  },
+  {
+    "revision": "6e665705921dc1bc4812",
+    "url": "/static/js/runtime-main.9c683482.js"
+  },
+  {
+    "revision": "05566166a459fa9a0484c42588c4b42a",
+    "url": "/static/media/404.05566166.jpg"
+  }
+]);
