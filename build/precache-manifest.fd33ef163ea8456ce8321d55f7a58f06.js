cb8f2a26a3183031ead27317e4fbe8f2e7ca9c25
s3lcuk <s3lcukdemir@gmail.com>
2020-03-06 15:36:07 +0300

    i don't know how to use git

---
+++ build/precache-manifest.fd33ef163ea8456ce8321d55f7a58f06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8b87549763177bbfca7c7168849ab18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a795324d5c2b5734a297",
+    "url": "/static/css/main.7e15a82a.chunk.css"
+  },
+  {
+    "revision": "473ac9b5280745672bf8",
+    "url": "/static/js/2.7850cbdd.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/static/js/2.7850cbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a795324d5c2b5734a297",
+    "url": "/static/js/main.951d3861.chunk.js"
+  },
+  {
+    "revision": "6e665705921dc1bc4812",
+    "url": "/static/js/runtime-main.9c683482.js"
+  },
+  {
+    "revision": "05566166a459fa9a0484c42588c4b42a",
+    "url": "/static/media/404.05566166.jpg"
+  }
+]);
